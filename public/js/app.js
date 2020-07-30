984893afbd9979080a0b6f66d08efec52439439d
Hakkym <akboiy50@gmail.com>
2020-07-30 04:05:22 -0700

    Removing unnessary console log call

--- public/js/app.js
+++ public/js/app.js
@@ -1,7 +1,3 @@
-
-console.log('hakkym')
-
-
 const webServerForm = document.querySelector('form');
 const search = document.querySelector('input');
 const messageOne = document.querySelector('#message-1')
